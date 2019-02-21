025e92cbda7e4de86e6c4d6cd4ca7aab6c24ff4f
LeslyN <lnombertoc@gmail.com>
2019-02-21 06:36:35 -0500

    Save information in the localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Services } from '@angular/core/src/view';
 
 @Component({
   selector: 'app-root',
@@ -8,19 +9,56 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'servicios-app';
 
-  services = [];
+  servicesList = [];
+  name: string;
+  description: string;
+
+  // editting = true;
+
+  constructor() {
+    // this.saveLocalStorage();
+  }
 
   addServices() {
+   this.servicesList.push(
+     {
+       name: this.name,
+       description: this.description,
+       index: this.servicesList.length
+     }
+   );
 
+    // Guardar los valores en el local storage
+    this.saveLocalStorage();
+  }
+
+  saveLocalStorage() {
+    localStorage.setItem('serviceList', JSON.stringify(this.servicesList));
   }
 
   editServices() {}
 
-  cancelServices() {}
+  cancelServices() {
 
-  deletedServices() {
+    // Dejará los valores como estaban antes, recuperar valores del local storage
+  }
 
+  deletedServices(index) {
+    let indexServ = this.listService.length;
+    const i = this.servicesList.indexOf(this.servicesList[indexServ]);
+    if (i !== -1) {
+      this.servicesList.splice(i, 1);
+    }
   }
 
   searchServices() {}
+
+
+
+
+  listService() {
+    // Recuperar los datos del local storage
+    let serviceLists = JSON.parse(localStorage.getItem("serviceList"));
+    console.log(serviceLists);
+  }
 }
